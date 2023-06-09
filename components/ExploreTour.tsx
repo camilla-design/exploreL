0a5bf4f8b19c910d23d84a5c965ce4407304039c
Atlungstad Brenneri <56583550+camilla-design@users.noreply.github.com>
2023-06-09 22:58:15 +0200

    added images to explore tour

--- components/ExploreTour.tsx
+++ components/ExploreTour.tsx
@@ -39,7 +39,7 @@ export default function ExploreTour({}: Props) {
       <div className=" mx-0 top-0 text-center overflow-hidden z-20 pt-5  ">
       <h1 className="text-3xl  text-[#ffa500] font-semibold ">Explore <span className='text-black font-bold'>Leka</span></h1>
     
-      <p className="p-3 pb-0 text-sm text-left ">Vandrer inn mot jordens indre og tråkker på 500 millioner år gamle bergarter, innover på biter av det Amerikanske kontinentet. Opp til Nordtjønna hvor vi spiser et godt måltid før vi tar turen videre opp til Utsikten. Herifra ser vi ned til Nesvatnet, utover Lekafjorden med Heilhornene majestetisk bak. Vi ser skipsleia Nordover forbi Torghatten, Vega og de syv søstre. Går ned til Møsmørklubban og bortover til Moho. Rundtur som krever god balanse og sterke ankler i et magisk, unikt og lunefullt landskap. </p>
+      <p className="p-3 pb-0 text-sm text-left ">Vandrer inn mot jordens indre og tråkker på 500 millioner år gamle bergarter, innover på biter av det Amerikanske kontinentet. Til Nordtjønna hvor vi spiser et godt måltid før vi tar turen videre opp til Utsikten. Ned til Nesvatnet, utover Lekafjorden med Heilhornene majestetisk bak. Vi ser skipsleia Nordover forbi Torghatten, Vega og de syv søstre. Så ned til Møsmørklubban og bortover til Moho. Krever god balanse og sterke ankler i et magisk, unikt og lunefullt landskap. </p>
       <p className="p-1 text-sm pt-2 font-bold">Pris: <span className='text-green-500'>990,-</span> pr pers. + <span className='text-green-500'>250,-</span> hvis det ønskes tilrettelagt for mat og drikke.</p>
       <button className="ctoaButton text-black border-2 border-white">Les Mer</button>
 
@@ -47,16 +47,16 @@ export default function ExploreTour({}: Props) {
     </div>
     <div className='grid grid-cols-2  border-2 border-black mt-2  '>
             <div className='...'>
-            <img src="explore-background.jpg" className="   " />
+            <img src="explore_1.jpg" className="   " />
             </div>
             <div className='...'>
-            <img src="explore-background.jpg" className="   " />
+            <img src="explore_2.jpg" className="   " />
             </div>
             <div className='...'>
-            <img src="explore-background.jpg" className="  " />
+            <img src="explore_3.jpg" className="  " />
             </div>
             <div className='...'>
-            <img src="explore-background.jpg" className=" " />
+            <img src="explore_4.jpg" className=" " />
             </div>
         </div>
    
